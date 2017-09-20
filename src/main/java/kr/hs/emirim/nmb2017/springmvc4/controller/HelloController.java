b6ee76c88e460764a8b612b371a87abd4593f24e
Hanbee Jang <hanbee990508@e-mirim.hs.kr>
2017-09-20 21:30:48 +0900

    Spring MVC 아키텍처

--- src/main/java/kr/hs/emirim/nmb2017/springmvc4/controller/HelloController.java
+++ src/main/java/kr/hs/emirim/nmb2017/springmvc4/controller/HelloController.java
@@ -1,13 +1,17 @@
 package kr.hs.emirim.nmb2017.springmvc4.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
 	 @RequestMapping("/") 
-	   public String hello() {
-	      return "resultPage";
-	   }
+	 public String hello(Model model) {
+		  model.addAttribute("message", "Hello from the controller"); 
+		  return "resultPage";
+		 }
+
+
 
 }
